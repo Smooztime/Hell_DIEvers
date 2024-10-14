955c6363fe91685000a21dd5b0fda19d54de9b72
Smooztime <chanakan.zoom@gmail.com>
2024-10-14 00:11:20 -0600

    Arm Rotate Fixed

--- Assets/Code/ControlArm.cs
+++ Assets/Code/ControlArm.cs
@@ -1,5 +1,7 @@
+using Code.Scripts.Player;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -10,32 +12,32 @@ public class ControlArm : MonoBehaviour
     Vector3 armPosition;
     [SerializeField]
     Transform armHolder;
-    Transform targetToPoint;
+    Vector3 targetToPoint;
     bool pointAtMouse;
     [SerializeField]
     float rotationSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        targetToPoint = armTransform;
-        armPosition = armHolder.position;
+        //targetToPoint = armTransform.position;
+        //armPosition = armHolder.position;
     }
     public void SetTargetToPoint(Vector2 pos)
     {
-       targetToPoint.position = pos;
+       targetToPoint = pos;
     }
 
     // Update is called once per frame
     void Update()
     {
-        armPosition = armHolder.position;
-        Vector3 aimDirection = targetToPoint.position - armHolder.position;
+        //armPosition = armHolder.position;
+        Vector3 aimDirection = targetToPoint - GetComponent<PlayerController>().RB.transform.position;
         aimDirection.Normalize();
         float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
        // Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
      //   Debug.Log("Angle " + angle + " " + targetRotation);
 
         armTransform.eulerAngles =new Vector3(0,0,angle + 90);
-        armTransform.position = armPosition;
+        //armTransform.position = armPosition;
     }
 }
